3dcb96b8f101839d74af4392a1700d937584cf3e
Hafiz feranmi <hafizferanmi@gmail.com>
2020-10-23 15:59:40 +0100

    Slight design changes/ MaterialTable added

--- cms/src/components/misc/Button/Button.js
+++ cms/src/components/misc/Button/Button.js
@@ -1,21 +1,31 @@
 import React from 'react'
-import styled from 'styled-components'
+import { makeStyles } from '@material-ui/core/styles'
 import MaterialButton from '@material-ui/core/Button'
 
-const StyledButton = styled(MaterialButton)`
-  box-shadow: none;
-  border-radius: 20px;
-`
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 40,
+    boxShadow: 'none',
+    color: 'white',
+    height: 42,
+    padding: '0 30px',
+    outline: 0
+  }
+})
 
 const Button = ({ label, ...props }) => {
+  const classes = useStyles()
   return (
-    <StyledButton
+    <MaterialButton
       variant='outlined'
       color='primary'
+      className={classes.root}
       {...props}
     >
       {label}
-    </StyledButton>
+    </MaterialButton>
   )
 }
 
