54eb8127d0222989427d2bd7e4e9cb880a63bff7
Hafiz feranmi <hafizferanmi@gmail.com>
2020-11-15 18:52:23 +0100

    *CMS: Login page

--- cms/src/components/LoginPage/Loginform.js
+++ cms/src/components/LoginPage/Loginform.js
@@ -1,63 +1,149 @@
 import React from 'react'
-import styled from 'styled-components'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers'
-import Button from '../misc/Button'
+import { makeStyles } from '@material-ui/styles'
+import { Person, RemoveRedEyeOutlined, Edit } from '@material-ui/icons'
 
 import ErrorMessage from '../misc/ErrorMessage'
-import Inputs from '../Inputs'
 import LoginFormSchema from './LoginFormSchema'
 
-const LoginFormWrapper = styled.form`
-  background-color: rgba(255 ,255 , 255, .7);
-  width: 360px;
-  padding: 20px 20px;
-  min-height: 200px;
-  position: relative;
-  overflow: hidden;
-  border-radius: 15px;
-`
+const useStyles = makeStyles({
+  loginFormWrapper: {
+    backgroundColor: 'rgba(255 ,255 , 255, 1)',
+    width: 350,
+    padding: 20,
+    minHeight: 200,
+    position: 'relative',
+    overflow: 'hidden',
+    borderRadius: 4,
+    border: 'solid 1px #0066f5',
+    // boxShadow: '0 10px 10px -5px black',
+    boxShadow: 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;'
+  },
+  signInText: {
+    fontSize: 30,
+    fontWeight: 500,
+    marginBottom: 20,
+    color: '#0c2e67'
+  },
+  buttonWrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    color: '#b7bcc0',
+    marginTop: 20
+  },
+  forgotPasswordText: {
+    marginTop: 10
+  },
+  button: {
+    background: '#0066f5',
+    borderRadius: 30,
+    color: 'white',
+    padding: '15px 25px',
+    fontSize: 13,
+    border: 0,
+    outline: 0,
+    cursor: 'pointer'
+  },
+  newAccountWrapper: {
+    marginTop: 30,
+    marginBottom: 20,
+    fontSize: 11,
+    textAlign: 'center',
+    color: '#0066f5'
+  },
+  iconInputWrapper: {
+    display: 'flex',
+    border: 'solid 1.5px #e0e2e5',
+    marginTop: 15
+  },
+  iconWrapper: {
+    color: ' #e0e2e5',
+    borderRight: 'solid 1.5px #e0e2e5',
+    marginLeft: 10,
+    paddingRight: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  input: {
+    border: 0,
+    outline: 0,
+    paddingBottom: 10,
+    padding: 10,
+    background: 'none',
+    width: '100%',
+    fontSize: 15,
+    marginLeft: 5,
+    paddingRight: 40,
+    color: 'black',
+    '&::placeholder': {
+      color: '#e0e2e5'
+    }
+  },
+  errorMessage: {
+    marginTop: 10
+  },
+  error: {
+    color: 'rgba(214, 10, 46)'
+  }
+})
 
 const Loginform = ({ serverState, submitForm }) => {
+  const classes = useStyles()
   const { register, handleSubmit, errors, formState } = useForm({
     resolver: yupResolver(LoginFormSchema)
   })
   const { isSubmitting } = formState
   const { message, serverError } = serverState
 
+  const [passwordVisible, setPasswordVisibility] = React.useState(false)
+  const handleEyeClick = () => {
+    setPasswordVisibility(!passwordVisible)
+  }
+
   return (
-    <LoginFormWrapper onSubmit={handleSubmit(submitForm)}>
+    <form className={classes.loginFormWrapper} onSubmit={handleSubmit(submitForm)}>
+      <p className={classes.signInText}>Sign in</p>
       <div>
-        <Inputs.TextInput
-          name='email'
-          type='email'
-          label='Email address'
-          register={register}
-          error={errors.email}
-          placeholder='Enter email'
-        />
-        <Inputs.TextInput
-          register={register}
-          name='password'
-          type='password'
-          label='Enter password'
-          error={errors.password}
-          placeholder='Enter password'
-        />
-        <div>
+        <div className={classes.iconInputWrapper}>
+          <div className={classes.iconWrapper}><Person /></div>
+          <input className={classes.input} type='email' ref={register} name='email' placeholder='email address' />
+        </div>
+        <div style={{ position: 'relative' }}>
+          <div className={classes.iconInputWrapper}>
+            <div className={classes.iconWrapper}><Edit /></div>
+            <input className={classes.input} type={passwordVisible ? 'text' : 'password'} ref={register} name='password' placeholder='password' />
+          </div>
+          <div onClick={handleEyeClick} style={{ position: 'absolute', top: '7px', right: '10px', fontSize: '6px', color: '#e0e2e5', cursor: 'pointer' }}>
+            <RemoveRedEyeOutlined />
+          </div>
+        </div>
+
+        <div className={classes.errorMessage}>
+          {errors && <p className={classes.error}>{errors && errors.email && errors.email.message}</p>}
+          {errors && <p className={classes.error}>{errors && errors.password && errors.password.message}</p>}
           <ErrorMessage
             networkError={serverError}
             message={message}
           />
         </div>
-        <Button
-          fullWidth
-          label='Login'
-          type='submit'
-          disabled={isSubmitting}
-        />
+        <div className={classes.buttonWrapper}>
+          <span className={classes.forgotPasswordText}>Forgot password ?</span>
+          <button
+            className={classes.button}
+            label='Login'
+            type='submit'
+            disabled={isSubmitting}
+          >
+            LOGIN
+          </button>
+        </div>
+        <div className={classes.newAccountWrapper}>
+          <span>New user? Create an account</span>
+        </div>
       </div>
-    </LoginFormWrapper>
+    </form>
   )
 }
 
--- cms/src/components/LoginPage/loginpage.js
+++ cms/src/components/LoginPage/loginpage.js
@@ -1,25 +1,82 @@
 import React from 'react'
 import Loginform from './LoginFormContainer'
-import styled from 'styled-components'
+import { makeStyles } from '@material-ui/styles'
+import { Typography } from '@material-ui/core'
 
 import loginformbackground from '../../assets/images/loginformbackground.jpg'
 
-const LoginPageWrapper = styled.div`
-  background-image: url(${loginformbackground});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+const useStyles = makeStyles({
+  loginPageWrapper: {
+    backgroundImage: `url(${loginformbackground})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    width: '100vw',
+    height: '100vh'
+  },
+  loginPageBackgroundFade: {
+    background: 'rgba(0, 0, 0, .7)',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    color: 'white'
+  },
+  loginFormWrapper: {
+    position: 'absolute',
+    top: '200px',
+    right: '150px'
+  },
+  welcomeBackWrapper: {
+    position: 'absolute',
+    left: '200px',
+    top: '150px',
+    fontSize: '64px',
+    fontWeight: 900,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: 1.38,
+    letterSpacing: 0.96,
+    '& p:first-of-type': {
+      opacity: '0.8'
+    },
+    '& p:last-of-type': {
+      opacity: '0.6'
+    }
+  },
+  agreementTextWrapper: {
+    position: 'absolute',
+    left: 200,
+    bottom: 150,
+    width: 400,
+    opacity: 0.4
+  }
+})
 
-const Loginpage = (props) => {
+const Loginpage = () => {
+  const classes = useStyles()
   return (
-    <LoginPageWrapper>
-      <Loginform />
-    </LoginPageWrapper>
+    <div className={classes.loginPageWrapper}>
+      <div className={classes.loginPageBackgroundFade}>
+        <div className={classes.loginFormWrapper}>
+          <Loginform />
+        </div>
+        <div className={classes.welcomeBackWrapper}>
+          <p>
+            Welcome Back,
+          </p>
+          <p>
+            Sign in to continue
+          </p>
+        </div>
+        <div className={classes.agreementTextWrapper}>
+          <Typography component='p' variant='body2'>
+            By continuing, you're confirming that you've read our Terms &amp; Conditions and Cookie Policy
+          </Typography>
+        </div>
+      </div>
+    </div>
   )
 }
 
