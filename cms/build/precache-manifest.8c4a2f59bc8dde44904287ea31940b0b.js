0a74c1991aec681dbb5a960d96a16650aa46f6c8
Hafiz feranmi <hafizferanmi@gmail.com>
2020-11-15 14:48:17 +0100

    *Frontend: Added build folder to git

---
+++ cms/build/precache-manifest.8c4a2f59bc8dde44904287ea31940b0b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52a5f19b38a6c28047622576b876d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11f8c76e80d5095c9500",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "57c8c81b8cd24c25cd91",
+    "url": "/static/css/main.c8ad1f89.chunk.css"
+  },
+  {
+    "revision": "11f8c76e80d5095c9500",
+    "url": "/static/js/2.7deaad8a.chunk.js"
+  },
+  {
+    "revision": "63f4fc43e40422a289fbc124e6f3c93c",
+    "url": "/static/js/2.7deaad8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57c8c81b8cd24c25cd91",
+    "url": "/static/js/main.c58a2b2a.chunk.js"
+  },
+  {
+    "revision": "2b8f580612459d80dc88",
+    "url": "/static/js/runtime-main.f65105ae.js"
+  },
+  {
+    "revision": "638ff98959dd5298b41b9296b552ac23",
+    "url": "/static/media/BRFirmaCW-Medium.638ff989.woff2"
+  },
+  {
+    "revision": "4556371f522de123940b4e47cff1b6af",
+    "url": "/static/media/loginformbackground.4556371f.jpg"
+  },
+  {
+    "revision": "94934930cf63cc4c28b7d7ac7946a142",
+    "url": "/static/media/raleway.94934930.woff2"
+  },
+  {
+    "revision": "b17e07dad0d9190e46f8a422fb372a79",
+    "url": "/static/media/restaurant.b17e07da.jpg"
+  }
+]);
