096fa59d5e3875a73fcee72e50e84df09f7a6058
Hafizferanmi <hafizferanmi@gmail.com>
2020-12-01 11:40:12 +0100

    closeable sidebar

--- cms/src/components/ManagersPage/ManagersPage.js
+++ cms/src/components/ManagersPage/ManagersPage.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import cn from 'clsx'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { makeStyles } from '@material-ui/core/styles'
 import Sidebar from '../Sidebar'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'
 
 const drawerWidth = 240
 
@@ -17,37 +20,69 @@ const useStyles = makeStyles((theme) => ({
   },
   aside: {
     background: 'white',
-    width: drawerWidth,
+    width: 70,
     position: 'fixed',
     top: 0,
     bottom: 0,
     height: '100%',
     zIndex: 20,
-    overflowY: 'auto'
+    overflowY: 'auto',
+    transition: 'width .05s'
   },
   content: {
-    marginLeft: drawerWidth,
+    marginLeft: 70,
     flexGrow: 1,
     height: '100vh',
     background: '#f4f8fc',
-    padding: '20px 40px'
+    padding: '20px 40px',
+    transition: 'margin-left .05s'
+  },
+  contentExpanded: {
+    marginLeft: drawerWidth
+  },
+  openSidebarButon: {
+    background: 'rgba(12, 46, 103, .4)',
+    position: 'fixed',
+    color: 'white',
+    top: 25,
+    left: 223,
+    cursor: 'pointer',
+    borderRadius: '50%',
+    width: 25,
+    height: 25,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 200,
+    '& svg': {
+      width: 15,
+      height: 15
+    }
+  },
+  openedSidebar: {
+    width: drawerWidth
   }
 }))
 
 const ManagersPage = (props) => {
   const { children } = props
   const classes = useStyles()
+  const [sideBarOpened, setSidebarOpen] = React.useState(true)
+  const toggleSidebar = () => setSidebarOpen(!sideBarOpened)
 
   return (
     <>
       <CssBaseline />
       <div className={classes.root}>
-        <aside className={classes.aside}>
+        <aside className={cn(classes.aside, sideBarOpened && classes.openedSidebar)}>
           <Sidebar />
         </aside>
-        <main className={classes.content}>
+        <main className={cn(classes.content, sideBarOpened && classes.contentExpanded)}>
           {children}
         </main>
+      </div>
+      <div onClick={toggleSidebar} className={classes.openSidebarButon}>
+        {sideBarOpened ? <NavigateNextIcon /> : <NavigateBeforeIcon />}
       </div>
     </>
   )
