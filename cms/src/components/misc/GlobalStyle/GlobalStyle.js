7170cb5cc84bf492a22767a4115cb5508be99f01
Hafizferanmi <hafizferanmi@gmail.com>
2021-01-05 12:33:17 +0100

    *CMS: fixed fonts and style

--- cms/src/components/misc/GlobalStyle/GlobalStyle.js
+++ cms/src/components/misc/GlobalStyle/GlobalStyle.js
@@ -4,7 +4,8 @@ const GlobalStyle = createGlobalStyle`
   *, body, input {
     font-family: Poppins;
     padding: 0;
-    margin: 0
+    margin: 0;
+    color: '#1c252c'
   }
   a {
     text-decoration: none;
